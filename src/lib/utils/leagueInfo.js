8758800d3102871a331af00522921ef9c035a127
EliasQFezzlewick <111521566+EliasQFezzlewick@users.noreply.github.com>
2023-05-14 23:08:27 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -27,7 +27,6 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 export const managers = [
     {
       "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "managerID" : "596699065010413568"
       "name": "Carl",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Currently in Rebuild Hell", // (optional)
